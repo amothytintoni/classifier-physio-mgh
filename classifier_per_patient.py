8d8c3839cd6aebd8aa4f691d414abe9a4476e8db
DESKTOP-425O0P1\Timothy Antoni <timothy.antoni@gmail.com>
2022-06-21 10:24:43 +0800

    2-binary confusion matrix: - no motion vs have motion - moderate vs high motion
    old code (3x3 cm) are commented. graphs work.

--- classifier_per_patient.py
+++ classifier_per_patient.py
@@ -40,6 +40,26 @@ df['activity_level'] = ((df['point_awake'])/(df['point_sleep']+df['point_awake']
 df = df[df['activity_level'].notna()]
 X = df.reset_index()['activity_level']
 y = df.reset_index()['Accl Validity']
+y_binary1 = np.zeros(len(y))
+y_binary2 = np.zeros(len(y))
+
+for i in range(0,len(y)):
+    if y[i] == 2:
+        y_binary1[i] = 1
+
+counterr=0
+m=0
+for i in range(0, len(y)-counterr):
+    if y[i] ==0:
+        y_binary2 = np.delete(y_binary2, i-counterr)
+        counterr+=1
+    else:
+        if y[i] == 1:
+            y_binary2[i-counterr] = 0
+        else:
+            y_binary2[i-counterr] = 1
+n_1v2 = len(y_binary2)
+#print(counterr)
 
 #clf = OneVsRestClassifier(SVC()).fit(X, y)
 #print(clf.score(X,y))
@@ -54,10 +74,11 @@ for i in range(0, len(y)):
     for j in range(0,n):
         y_2d[i][j] = y[i]
 
-cm0 = np.empty((len(threshold_01),3,3),dtype = float)
-cm1 = np.empty((int(n*(n-1)/2),3,3),dtype = float)
-cm2 = np.empty((len(threshold_01),3,3),dtype = float)
-cm = np.empty((len(threshold_01),3,3),dtype = float)
+cm0 = np.zeros((len(threshold_01),3,3))
+cm1 = np.zeros((int(n*(n-1)/2),3,3),)
+cm2 = np.zeros((len(threshold_01),3,3))
+cm0v1 = np.zeros((n,2,2))
+cm1v2 = np.zeros((n_1v2,2,2))
 
 delta_x = threshold_01[1] - threshold_01[0]
 counter=0
@@ -66,25 +87,146 @@ y_pred1 = np.zeros(len(y))
 y_pred2 = np.zeros(len(y))
 y_pred = np.zeros(len(y))
 y_pred02d = np.zeros((len(y), len(threshold_01)))
+y_pred0v1 = np.zeros(len(y))
+y_pred1v2 = np.zeros(len(y_binary2))
 
 
 #print(X[0])
 fp0 = np.empty(len(threshold_01), dtype = float)
 tp0 = np.empty(len(threshold_01), dtype = float)
-fp1 =np.empty(int(n*(n-1)/2), dtype = float)
-tp1 = np.empty(int(n*(n-1)/2), dtype = float)
+#fp1 =np.empty(int(n*(n-1)/2), dtype = float)
+#tp1 = np.empty(int(n*(n-1)/2), dtype = float)
 fp2 =np.empty(len(threshold_01), dtype = float)
 tp2 = np.empty(len(threshold_01), dtype = float)
 fn0 =np.empty(len(threshold_01), dtype = float)
 tn0 =np.empty(len(threshold_01), dtype = float)
 fn2 =np.empty(len(threshold_01), dtype = float)
 tn2 =np.empty(len(threshold_01), dtype = float)
-fn1 =np.empty(int(n*(n-1)/2), dtype = float)
-tn1 = np.empty(int(n*(n-1)/2), dtype = float)
+#fn1 =np.empty(int(n*(n-1)/2), dtype = float)
+#tn1 = np.empty(int(n*(n-1)/2), dtype = float)
+
+fp0v1 = np.empty(len(threshold_01), dtype = float)
+tp0v1 = np.empty(len(threshold_01), dtype = float)
+fp1v2 =np.empty(n, dtype = float)
+tp1v2 = np.empty(n, dtype = float)
+fn0v1 =np.empty(len(threshold_01), dtype = float)
+tn0v1 =np.empty(len(threshold_01), dtype = float)
+fn1v2 =np.empty(n, dtype = float)
+tn1v2 = np.empty(n, dtype = float)
+
+#for no motion vs have motion
+counter=0
+for i in threshold_01:
+    for j in range(0, len(X)):
+        if X[j]<=i:
+            y_pred0v1[j] = 0
+            #y_pred02d[j][isub] = 0
+        else:
+            y_pred0v1[j] = 1
+            #y_pred02d[j][isub] = 1
+    cm0v1[counter] = confusion_matrix(y_binary1, y_pred0v1)
+    counter+=1
+
+for i in range(0,len(threshold_01)):
+    fn0v1[i] = cm0v1[i][1][0]
+    tn0v1[i] = cm0v1[i][0][0]
+    fp0v1[i] = cm0v1[i][0][1]
+    tp0v1[i] = cm0v1[i][1][1]
+
+fp0v1/=fp0v1.max()
+tp0v1/=tp0v1.max()
+fn0v1/=fn0v1.max()
+tn0v1/=tn0v1.max()
+sens0v1 = tp0v1/(fn0v1+tp0v1)
+spec0v1 = tn0v1/(tn0v1+fp0v1)
+#aa = metrics.multilabel_confusion_matrix(y_2d, y_pred02d)
+
+fp0v1 = np.append(fp0v1, 0)
+tp0v1 = np.append(tp0v1, 0)
+sens0v1 = np.append(sens0v1, 0)
+spec0v1 = np.append(spec0v1, 1)
+sens0v1[0] = 1 
+spec0v1[0] = 0 
+sens1v2 = np.append(sens0v1, 0)
+spec1v2 = np.append(spec0v1, 1)
+
+plt.figure(3)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.plot(fp0v1,tp0v1, 'r-', label = 'no vs have motion')
+plt.title('ROC (TP vs FP)')
+plt.legend()
+
+plt.figure(4)
+plt.plot(spec0v1,sens0v1, 'r-', label = 'no vs have motion')
+plt.xlabel('Specificity')
+plt.ylabel('Sensitivity')
+plt.title('ROC (Sens vs Spec)')
+plt.legend()
+
+area0v1 = auc(fp0v1,tp0v1)
+
+
+print(area0v1)
+
+
+#for moderate vs high motion
+counter=0
+for i in threshold_12:
+    for j in range(0, n_1v2):
+        if X[j]>=i:
+            y_pred1v2[j] = 1
+            #y_pred02d[j][isub] = 0
+        else:
+            y_pred1v2[j] = 0
+            #y_pred02d[j][isub] = 1
+    cm1v2[counter] = confusion_matrix(y_binary2, y_pred1v2)
+    counter+=1
+
+for i in range(0,len(threshold_12)):
+    fn1v2[i] = cm1v2[i][1][0]
+    tp1v2[i] = cm1v2[i][1][1]
+    fp1v2[i] = cm1v2[i][0][1]
+    tn1v2[i] = cm1v2[i][0][0]
+
+fp1v2/=fp1v2.max()
+tp1v2/=tp1v2.max()
+fn1v2/=fn1v2.max()
+tn1v2/=tn1v2.max()
+sens1v2 = tp1v2/(fn1v2+tp1v2)
+spec1v2 = tn1v2/(tn1v2+fp1v2)
+#aa = metrics.multilabel_confusion_matrix(y_2d, y_pred02d)
+
+fp1v2 = np.append(fp1v2, 0)
+tp1v2 = np.append(tp1v2, 0)
+
+plt.figure(3)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.plot(fp1v2,tp1v2, 'g-', label = 'moderate vs high motion')
+plt.title('ROC (TP vs FP)')
+plt.legend()
+
+plt.figure(4)
+plt.plot(spec1v2,sens1v2, 'g-', label = 'moderate vs high motion')
+plt.xlabel('Specificity')
+plt.ylabel('Sensitivity')
+plt.title('ROC (Sens vs Spec)')
+plt.legend()
+
+area1v2 = auc(fp1v2,tp1v2)
+
+
+print(area1v2)
+
+
 
 
 #for lower threshold, 0-1
 #isub=0
+
+"""
+counter=0
 for i in threshold_01:
     for j in range(0, len(X)):
         if X[j]<i:
@@ -96,22 +238,6 @@ for i in threshold_01:
     cm0[counter] = confusion_matrix(y, y_pred0)
     counter+=1
     #isub+=1
-
-"""
-#    try
-counter=0
-for i in threshold_12:
-    for j in range(0,len(X)):
-        if X[j]> i:
-            y_pred1[j] = 2
-        
-    cm1[counter] = confusion_matrix(y, y_pred1)
-    counter+=1
-#
-for i in range(0,len(threshold_01)):
-    for j in range(0,3):
-        cm0[i][j][2] = cm1[i][j][2]
-"""
     
 for i in range(0,len(threshold_01)):
     fp0[i] = cm0[i][1][0] + cm0[i][2][0]
@@ -147,7 +273,7 @@ area0 = auc(fp0,tp0)
 
 print(area0)
 
-
+"""
 #for 1
 """
 counter=0
@@ -198,7 +324,7 @@ area1 = auc(df2['fp1'],df2['tp1'])
 #print(area0)
 print(area1)
 """
-
+"""
 #for 2 - higher threshold, between 1-2
 counter=0
 for i in threshold_12:
@@ -220,8 +346,16 @@ fp2/=fp2.max()
 tp2/=tp2.max()
 fn2/=fn2.max()
 tn2/=tn2.max()
+
+fp2 = np.append(fp2,0)
+tp2 = np.append(tp2,0)
+
+fn2 = np.append(fn2,1)
+tn2 = np.append(tn2,1)
+
 sens2 = tp2/(fn2+tp2)
 spec2 = tn2/(tn2+fp2)
+
 
 plt.figure(1)
 plt.xlabel('False Positive Rate')
@@ -238,22 +372,26 @@ plt.legend()
 area2 = auc(fp2,tp2)
 
 print(area2)
-
+"""
+threshold_01 = np.append(threshold_01, 100)
+spec0v1 = np.delete(spec0v1,100)
 
 fig, ax1 = plt.subplots()
 
 color = 'tab:red'
 ax1.set_xlabel('threshold')
 ax1.set_ylabel('sensitivity', color=color)
-ax1.plot(sens0, threshold_01, color=color)
+ax1.plot(sens0v1, threshold_01, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 plt.title('Sensitivity and Specificity vs 0-1 Activity threshold')
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
+threshold_01 = np.delete(threshold_01, 100)
+
 color = 'tab:green'
 ax2.set_ylabel('specificity', color=color)  # we already handled the x-label with ax1
-ax2.plot(spec0, threshold_01, color=color)
+ax2.plot(spec0v1, threshold_01, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
@@ -261,19 +399,21 @@ plt.show()
 
 
 fig, ax1 = plt.subplots()
+#threshold_01 = np.delete(threshold_01, 100)
+threshold_12 = np.append(threshold_12, 100)
 
 color = 'tab:red'
 ax1.set_xlabel('threshold')
 ax1.set_ylabel('sensitivity', color=color)
-ax1.plot(sens2, threshold_01, color=color)
+ax1.plot(sens1v2, threshold_01, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
-plt.title('Sensitivity and Specificity vs 0-1 Activity threshold')
+plt.title('Sensitivity and Specificity vs 1-2 Activity threshold')
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:green'
 ax2.set_ylabel('specificity', color=color)  # we already handled the x-label with ax1
-ax2.plot(spec2, threshold_01, color=color)
+ax2.plot(spec1v2, threshold_01, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
